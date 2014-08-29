bbb0883554d27e08cce5360a0769588dc0daac29
ddujisin <daniel@syncrew.com>
2014-08-29 21:14:58 +0000

    dev changes

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Depot::Application.initialize!
-
+ENV['RAILS_ENV'] ||= 'production'
 
 Depot::Application.configure do
 
